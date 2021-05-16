7b6f19303d884232d2ede512756ac7c615174142
erennaltin <erennaltin@gmail.com>
2021-05-16 21:46:57 +0300

    changed set

--- cipbackend/settings.py
+++ cipbackend/settings.py
@@ -170,7 +170,7 @@ GRAPHQL_AUTH = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-STATIC_URL = BASE_DIR / "/static/"
+STATIC_URL = "/static/"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media/"
